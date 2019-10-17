b5ba90483fa9d3a33881e74347ea34029a6fbd71
Andromeda <hannalauth@gmail.com>
2019-10-17 11:22:21 -0700

    Version-3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 
 // HTML
-const displayHTML = require('./utils/displayHTML'); 
+const displayHTML = require('./utils/displayHTML');
 
 // Classes
 const Manager = require('./lib/Manager');
@@ -19,31 +19,31 @@ const {
 } = require('./lib/questions');
 
 
-// Asks questions and returns Manager {name, id, email, officeNumber}
-async function getManager() {
-    try {
-        const { name, id, email, officeNumber } = await inquirer.prompt(employeeQuestions.concat(managerQuestions));
-        return new Manager(name, id, email, officeNumber)
-    } catch (err) {
-        throw err
-    }
-}
+async function getSingleEmployee(employeeType) {
 
-// Asks questions and returns Engineer {name, id, email, github}
-async function getEngineer() {
-    try {
-        const { name, id, email, githubUsername } = await inquirer.prompt(employeeQuestions.concat(engineerQuestions));
-        return new Engineer(name, id, email, githubUsername)
-    } catch (err) {
-        throw err
-    }
-}
+    let Klass;
+    let questions;
 
-// Asks questions and returns Intern {name, id, email, school}
-async function getIntern() {
+    switch (employeeType) {
+        case 'Engineer':
+            Klass = Engineer;
+            questions = engineerQuestions
+            break;
+        case 'Intern':
+            Klass = Intern;
+            questions = internQuestions
+            break;
+        case 'Manager':
+            Klass = Manager;
+            questions = managerQuestions
+            break;
+    }
+
     try {
-        const { name, id, email, school } = await inquirer.prompt(employeeQuestions.concat(internQuestions));
-        return new Intern(name, id, email, school)
+
+        let data = await inquirer.prompt(employeeQuestions.concat(questions));
+        return new Klass(data)
+
     } catch (err) {
         throw err
     }
@@ -51,14 +51,14 @@ async function getIntern() {
 
 // Returns an array of all team members
 async function createTeam() {
-    
+
     const team = [];
 
     return new Promise(async (resolve, reject) => {
         try {
 
             // Get manager and push to team array
-            let manager = await getManager();
+            let manager = await getSingleEmployee("Manager");
             team.push(manager);
 
             // Get next employee
@@ -66,33 +66,30 @@ async function createTeam() {
 
             async function getEmployee() {
                 try {
-                inquirer
-                    .prompt(employeeTypeQuestions)
-                    .then(async ({ employeeType }) => {
-                        console.log(employeeType);
-                        if (employeeType === "Engineer") {
-                            return { engineer } = await getEngineer();
-                        } else if (employeeType === "Intern") {
-                            return { intern } = await getIntern();
-                        } else {
-                            employee = null
-                        }
-                        // whatever you "return" from a .then is a resolved promise
-                    }).then(employee => {
-                        if (employee) {
-                            team.push(employee);
-                            getEmployee();
-                        } else {
-                            resolve(team)
-                        }
-                    }).catch(err => console.log(err))
-
+                    inquirer
+                        .prompt(employeeTypeQuestions)
+                        .then(async ({ employeeType }) => {
+                            if (employeeType === "Engineer") {
+                                return { engineer } = await getSingleEmployee(employeeType);
+                            } else if (employeeType === "Intern") {
+                                return { intern } = await getSingleEmployee(employeeType);
+                            } else {
+                                employee = null
+                            }
+                        }).then(employee => {
+                            if (employee) {
+                                team.push(employee);
+                                getEmployee();
+                            } else {
+                                resolve(team)
+                            }
+                        })
                 } catch (err) {
                     reject(err)
                 }
             }
-            
-    
+
+
         } catch (err) {
             console.log(err)
         }
