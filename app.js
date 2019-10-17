00f5e9d90302f9071ca732760700e0cec4adcb1e
Andromeda <hannalauth@gmail.com>
2019-10-17 11:16:22 -0700

    Version-2

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Dependencies
 const inquirer = require('inquirer');
 
+// HTML
+const displayHTML = require('./utils/displayHTML'); 
+
 // Classes
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
@@ -8,68 +11,98 @@ const Intern = require('./lib/Intern');
 
 // Questions
 const {
-    menuQuestions, 
-    employeeQuestions, 
-    managerQuestions, 
-    engineerQuestions, 
+    employeeTypeQuestions,
+    employeeQuestions,
+    managerQuestions,
+    engineerQuestions,
     internQuestions
 } = require('./lib/questions');
 
-// HTML
-const displayHtml = require('./utils/displayHtml');
 
-// Employees
-const employeeList = [];
-
-// Functions
-function init() {
-    console.log('\nWelcome! Time to build your team:\n')
-    inquirer
-    .prompt(employeeQuestions.concat(managerQuestions))
-    .then(({name, id, email, officeNumber}) => {
-        let manager = new Manager(name, id, email, officeNumber);
-        employeeList.push(manager);
-        getEmployee();
-    })
+// Asks questions and returns Manager {name, id, email, officeNumber}
+async function getManager() {
+    try {
+        const { name, id, email, officeNumber } = await inquirer.prompt(employeeQuestions.concat(managerQuestions));
+        return new Manager(name, id, email, officeNumber)
+    } catch (err) {
+        throw err
+    }
 }
 
-function getEmployee() {
-    inquirer
-    .prompt(menuQuestions)
-    .then(({userChoice}) => {
-        switch (userChoice) {
-            case 'Engineer':
-                getEngineer();
-                break;
-            case 'Intern':
-                getIntern();
-                break;
-            case 'Exit':
-                displayHtml(employeeList);
-                break;
+// Asks questions and returns Engineer {name, id, email, github}
+async function getEngineer() {
+    try {
+        const { name, id, email, githubUsername } = await inquirer.prompt(employeeQuestions.concat(engineerQuestions));
+        return new Engineer(name, id, email, githubUsername)
+    } catch (err) {
+        throw err
+    }
+}
+
+// Asks questions and returns Intern {name, id, email, school}
+async function getIntern() {
+    try {
+        const { name, id, email, school } = await inquirer.prompt(employeeQuestions.concat(internQuestions));
+        return new Intern(name, id, email, school)
+    } catch (err) {
+        throw err
+    }
+}
+
+// Returns an array of all team members
+async function createTeam() {
+    
+    const team = [];
+
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            // Get manager and push to team array
+            let manager = await getManager();
+            team.push(manager);
+
+            // Get next employee
+            await getEmployee();
+
+            async function getEmployee() {
+                try {
+                inquirer
+                    .prompt(employeeTypeQuestions)
+                    .then(async ({ employeeType }) => {
+                        console.log(employeeType);
+                        if (employeeType === "Engineer") {
+                            return { engineer } = await getEngineer();
+                        } else if (employeeType === "Intern") {
+                            return { intern } = await getIntern();
+                        } else {
+                            employee = null
+                        }
+                        // whatever you "return" from a .then is a resolved promise
+                    }).then(employee => {
+                        if (employee) {
+                            team.push(employee);
+                            getEmployee();
+                        } else {
+                            resolve(team)
+                        }
+                    }).catch(err => console.log(err))
+
+                } catch (err) {
+                    reject(err)
+                }
+            }
+            
+    
+        } catch (err) {
+            console.log(err)
         }
     })
 }
 
-function getEngineer() {
-    inquirer
-    .prompt(employeeQuestions.concat(engineerQuestions))
-    .then(({name, id, email, githubUsername}) => {
-        let engineer = new Engineer(name, id, email, githubUsername);
-        employeeList.push(engineer);
-        getEmployee();
-    })
+async function test() {
+    let finalTeam = await createTeam();
+    console.log(finalTeam);
+    displayHTML(finalTeam);
 }
 
-function getIntern() {
-    inquirer
-    .prompt(employeeQuestions.concat(internQuestions))
-    .then(({name, id, email, school}) => {
-        let intern = new Intern(name, id, email, school);
-        employeeList.push(intern)
-        getEmployee();
-    })
-}
-
-// Methods
-init();
+test();
